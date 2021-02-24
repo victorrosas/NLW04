70fe1520e91bf1101d971b248e894f5ad5633a1e
Victor Rosas <victor.rosas.96@hotmail.com>
2021-02-24 18:14:25 -0500

    fineshed third day

--- src/components/ChallgenBox.tsx
+++ src/components/ChallgenBox.tsx
@@ -3,28 +3,26 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallgenBox.module.css';
 
 export function ChallgenBox() {
-    const contextData = useContext(ChallengesContext);
+    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
-    console.log(contextData)
-
-    const hasActiveChallenge = true;
 
     return (
         <div className={styles.challengeBoxContainer}>
-            { hasActiveChallenge ? (
+            { activeChallenge ? (
                 <div className={styles.challengeActive}>
-                    <header>Ganhe 400 xp</header>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
 
                     <main>
-                        <img src="icons/body.svg" />
+                        <img src={`icons/${activeChallenge.type}.svg`} />
                         <strong>Novo desafio</strong>
-                        <p>Levante e faça a caminhada de 3 minutos.</p>
+                        <p>{activeChallenge.description}</p>
                     </main>
 
                     <footer>
                         <button
                          type="button"
                          className={styles.challengeFailedButton}
+                         onClick={resetChallenge}
                          >
                             Falhei
                         </button>
